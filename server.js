bd9cb66832ed15b7d588c585b04f5bfeb86af50b
Asifiwe BUHENDWA <mickaelbhd@gmail.com>
2023-10-04 23:25:49 +0200

    Initial commit2

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3001; // You can change the port number to any available port you prefer
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+// Define a basic route
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
 });
 
-server.listen(3001, () => {
-  console.log('Server running at http://localhost:3001/');
+// Middleware to parse JSON data
+app.use(bodyParser.json());
+
+// Route to handle form data submission
+app.post('/api/submit-reservation', (req, res) => {
+  const formData = req.body;
+  console.log('Received data:', formData);
+
+  // Process the data and send a response
+  // You can save it to a database, perform validation, etc.
+
+  res.status(200).json({ message: 'Data received successfully' });
+});
+
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
