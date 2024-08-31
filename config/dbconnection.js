cab3a675109fa8c897e753b4b7f899cb1d936c61
Asifiwe BUHENDWA <mickaelbhd@gmail.com>
2024-08-31 21:35:47 +0200

    Updating connection to the DB

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost:27017/bar-restau")
 
-
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
